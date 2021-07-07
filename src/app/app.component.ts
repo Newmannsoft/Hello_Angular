73b8b5485c946244570cb9a802c5492d04432069
Stuwar Newmann <newmannsoft@gmail.com>
2021-07-06 20:15:42 -0500

    001

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { Product } from './product.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ecomerce';
+
+  items = ['Arnold', 'Rafa', 'Pedro', 'Juliana'];
+
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/001.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/002.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/003.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/004.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/005.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/006.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addItem() {
+    this.items.push('Nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+}
---
+++ src/app/product.model.ts
@@ -0,0 +1,8 @@
+// interface
+export interface Product {
+    id: string;
+    title: string;
+    image: string;
+    price: number;
+    description: string;
+}
